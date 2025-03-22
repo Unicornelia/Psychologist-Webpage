aced276e866fe012aa6c45000d5d720978693712
Unicornelia <ez.kornelia@gmail.com>
2025-03-22 19:08:23 +0100

    fix: dependencies and p[ackages

---
+++ public/component---src-pages-about-tsx-73a6cfd642f2db97a9ac.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunkcsabaeszter=self.webpackChunkcsabaeszter||[]).push([[970],{4037:function(e,a,t){a.A=t.p+"static/pastelshapes2-d29772ce87badbda90f403f5b599091c.jpg"},5492:function(e,a,t){t.r(a),t.d(a,{default:function(){return d}});var l=t(9605),s=t(4037),r=t.p+"static/profil-44ff6618284881cf84e3a507ab2cd975.jpg",n=t(6899),i=t(1250),o=t(9331);const g=i.Ay.main`
+  background-image: url(${s.A});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  font-weight: 500;
+  margin: 0;
+  min-height: 100vh;
+  padding: 5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`,z=i.Ay.img`
+  display: flex;
+  margin: 2rem auto;
+  max-width: 50vw;
+  border: 1px solid grey;
+  border-radius: 2px;
+`;var d=()=>(0,o.Y)(l.A,null,(0,o.Y)(g,null,(0,o.Y)(n.A,null,(0,o.Y)("span",null,"Csaba Eszter vagyok, pszichológus, tanár és edző. Pszichológus végzettségem 2010-ben az Eötvös Loránd Tudományegyetemen szereztem. Módszerspecifikus képzésem a Magyar Családterápiás Egyesületnél, illetve a Nemzetközi Sématerápiás Egyesület kereteiben végzem."),(0,o.Y)("p",null,"Diplomáim kézhezvételét követően az állami ellátásban szereztem tapasztalatot gyerekek, felnőttek és családok pszichológiai támogatásával, szakszerű segítésével."),(0,o.Y)("p",null,"A segítő szakmában eltöltött évek alatt szerzett gyakorlat, az eközben szélesedő-bővülő eszköztáram, illetve a szakmai közeg támogató segítségével váltam magánzó szakemberré."),(0,o.Y)("div",null,(0,o.Y)("p",null,"Várom Budapesten szeretettel a választ-, támogatást kereső felnőtteket, családokat. Motivációm, hogy a hozzám fordulók képessé váljanak:"),(0,o.Y)("ul",null,(0,o.Y)("li",null,"szükségleteik felismerésére, kifejezésére"),(0,o.Y)("li",null,"az önmagukról való gondoskodásra."))),(0,o.Y)(z,{src:r,alt:"profile"}))))},6899:function(e,a,t){var l=t(1250),s=t(9331);const r=l.Ay.article`
+  font-weight: 500;
+  margin: 40px auto;
+  padding: 30px;
+  line-height: 1.7;
+  text-align: justify;
+  width: 70%;
+  border-radius: 1px;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.5);
+  -moz-box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.5);
+  -webkit-box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.5);
+`;a.A=e=>{let{children:a}=e;return(0,s.Y)(r,null,a)}}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-73a6cfd642f2db97a9ac.js.map
---
+++ public/component---src-pages-index-tsx-1994cc2bfd82693c0099.js
@@ -0,0 +1,34 @@
+"use strict";(self.webpackChunkcsabaeszter=self.webpackChunkcsabaeszter||[]).push([[245],{4037:function(e,a,t){a.A=t.p+"static/pastelshapes2-d29772ce87badbda90f403f5b599091c.jpg"},6899:function(e,a,t){var n=t(1250),l=t(9331);const r=n.Ay.article`
+  font-weight: 500;
+  margin: 40px auto;
+  padding: 30px;
+  line-height: 1.7;
+  text-align: justify;
+  width: 70%;
+  border-radius: 1px;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.5);
+  -moz-box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.5);
+  -webkit-box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.5);
+`;a.A=e=>{let{children:a}=e;return(0,l.Y)(r,null,a)}},9754:function(e,a,t){t.r(a),t.d(a,{default:function(){return p}});var n=t(1250),l=t(4037),r=t(9605),s=t.p+"static/therapy2-f07e9852ede54c5f8f367394d8bda42e.jpg",i=t(6899),o=t(9331);const g=n.Ay.main`
+  background-image: url(${l.A});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  font-weight: 500;
+  margin: 0;
+  min-height: 100vh;
+  padding: 5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`,d=n.Ay.img`
+  border: 1px solid grey;
+  display: flex;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 600px;
+  height: auto;
+`;var p=()=>(0,o.Y)(r.A,null,(0,o.Y)(g,null,(0,o.Y)(i.A,null,(0,o.Y)("p",null,"Üdvözlöm a holnapomon! A jó élet egy folyamat, nem létező állapot –mondja Carl Rogers, a humanista pszichológia atyja."),(0,o.Y)("p",null,"Ezzel üzeni nekünk, hogy a növekedés, az önmegvalósítás nem egy egyszeri célon alapuló statikus állapot."),(0,o.Y)("p",null,"A fejlődés maga az út, és a “jó élet” törekvés a fejlődésre. Amennyiben segítő szakembert keres, aki partnerközpontú szemléletben kíséri, hogy megtalálja a kérdésére a saját válaszait, kíséri a fejlődése útján, és kíséri abban, hogy megtanulja saját szükségleteit felismerni és betölteni, jó helyen jár."),"Vallom, hogy minden kérdés jó, fontos és számít. Ezért kérem, írjon bátran üzenetet!"),(0,o.Y)(d,{src:s,alt:"therapy"})))}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-1994cc2bfd82693c0099.js.map
