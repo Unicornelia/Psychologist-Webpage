4a7cb25cd2ae378d66fb30c3531fcd8962ea53b8
Unicornelia <ez.kornelia@gmail.com>
2026-02-01 17:22:36 +0100

    fix: remove intersection observer and not needed config for polyfill

--- src/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// ES6 way
-export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (!('IntersectionObserver' in window)) {
-    import('react-intersection-observer');
-    console.log('# IntersectionObserver is polyfilled!');
-  }
-};
